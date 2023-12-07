14edd78312af62ee7c35ab0d8cb5d20e9d5df194
gabriel.rezende <gabrielpires763@gmail.com>
2023-12-07 17:55:33 -0300

    Remove second activity and add listview to main activity

--- app/src/main/java/com/example/basicandroidmqttclient/MainActivity.java
+++ app/src/main/java/com/example/basicandroidmqttclient/MainActivity.java
@@ -4,9 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.content.Intent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.hivemq.client.mqtt.datatypes.MqttQos;
@@ -14,14 +15,19 @@ import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.basicandroidmqttclient.MESSAGE";
-    public static final String brokerURI = "3.223.10.115";
+    public static final String brokerURI = "54.243.87.127";
 
     Activity thisActivity;
     TextView subMsgTextView;
+
+    private ListView listViewSubscribedMessages;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> subscribedMessages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +35,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         thisActivity = this;
-        subMsgTextView = (TextView) findViewById(R.id.editTextMultiLineSubMsg);
+        thisActivity = this;
+        listViewSubscribedMessages = findViewById(R.id.listViewSubscribedMessages);
+
+        subscribedMessages = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, subscribedMessages);
+        listViewSubscribedMessages.setAdapter(adapter);
     }
 
-    /** Called when the user taps the Send button */
+    /**
+     * Called when the user taps the Send button
+     */
     public void publishMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText topicName = (EditText) findViewById(R.id.editTextTopicName);
         EditText value = (EditText) findViewById(R.id.editTextValue);
 
@@ -50,15 +62,10 @@ public class MainActivity extends AppCompatActivity {
                 .payload(value.getText().toString().getBytes())
                 .send();
         client.disconnect();
-
-        String message = topicName.getText().toString() + " " + value.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 
     public void sendSubscription(View view) {
-        EditText topicName = (EditText) findViewById(R.id.editTextTopicNameSub);
-
+        EditText topicName = findViewById(R.id.editTextTopicNameSub);
         Mqtt5BlockingClient client = Mqtt5Client.builder()
                 .identifier(UUID.randomUUID().toString())
                 .serverHost(brokerURI)
@@ -66,20 +73,16 @@ public class MainActivity extends AppCompatActivity {
 
         client.connect();
 
-        // Use a callback lambda function to show the message on the screen
         client.toAsync().subscribeWith()
                 .topicFilter(topicName.getText().toString())
                 .qos(MqttQos.AT_LEAST_ONCE)
                 .callback(msg -> {
-                    thisActivity.runOnUiThread(new Runnable() {
-                        public void run() {
-                            subMsgTextView.setText(new String(msg.getPayloadAsBytes(), StandardCharsets.UTF_8));
-                        }
+                    String receivedMessage = new String(msg.getPayloadAsBytes(), StandardCharsets.UTF_8);
+                    thisActivity.runOnUiThread(() -> {
+                        subscribedMessages.add(msg.getTopic() + ": " + receivedMessage);
+                        adapter.notifyDataSetChanged();
                     });
                 })
                 .send();
     }
-
-
-
 }
